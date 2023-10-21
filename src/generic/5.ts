34855b92f1e96a5a421c6f955e244cc2596a8590
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-10-21 17:19:39 +0300

    done 5-8

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,9 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair <T, A>{
+  key: T;
+  value: A;
 }
 
 export {};
